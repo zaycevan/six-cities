4dcaf6757a269f97f79d20dd311a3c31c47e0b97
Natalia Zaytseva <zaycevan@gmail.com>
2020-10-03 21:31:47 +0500

    Добавляет описание маршрутов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import RoomScreen from "../room-screen/room-screen";
 
 const App = (props) => {
   const {placesCount} = props;
 
   return (
-    <MainScreen placesCount={placesCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen placesCount={placesCount} />
+        </Route>
+        <Route exact path="/login">
+          <SignInScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <RoomScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
